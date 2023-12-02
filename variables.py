1eafe6dcc70a725325338b8fef92f51c1d2b75ef
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2023-12-02 18:01:12 +0530

    learned variables

---
+++ variables.py
@@ -0,0 +1,12 @@
+myName = "Ankur Halder"
+my_name = "Ankur Halder"
+x = 1
+y = "hello"
+a = b = c = 10
+x, y, z = 1, 2, "Ankur"
+print(x , y , a , b , c , my_name , myName , x, y, z)
+
+x =20
+print(x)
+del x
+print(x)
