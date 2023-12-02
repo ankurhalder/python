954fdc6da16750e58c162a7c73912fb990a46cf1
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2023-12-02 18:22:47 +0530

    learned strings

---
+++ strings.py
@@ -0,0 +1,17 @@
+a = 'hello'
+b = "hello"
+print (a)
+print (b)
+
+c = 'this is a single line string'
+print (c)
+d = 'this is a multil'\
+    'ine string'
+print (d)
+
+e = "hi"
+f = "hello"
+print (e+f)
+name = "Ankur"
+print (name[0])
+print (len(name))
