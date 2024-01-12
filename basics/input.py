6496d43d37268cccb091d92064179baf6f7a7e03
Ankur Halder <115826489+ankurhalder@users.noreply.github.com>
2024-01-12 14:47:05 +0530

    learned inputs and type casting

---
+++ basics/input.py
@@ -0,0 +1,10 @@
+num = input('Please enter a number:')
+num2 = input('Please enter another number:')
+
+print(num + num2)
+print(int(num) + int(num2))
+
+
+first_name = input('Please enter your first name:')
+last_name = input('Please enter your last name:')
+print('Hello ' + first_name + ' ' + last_name)
